a45afe14f87313a9ae3036f4e6c5633f59e0b749
Sean Hong <iamseanhong@gmail.com>
2018-12-24 10:07:20 -0700

    Solve reversestring

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,28 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  // start with output str ''
+  // for each char
+  // outputStr = char + outputStr
+
+  // ex 'abcd'
+
+  // char 0 = a
+  // outputStr = a + ''
+  // outputStr = a
+
+  // char 1 = b
+  // outputStr = b + a
+  // outputStr = ba
+
+  // ...
+  let outputStr = '';
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    outputStr = char + outputStr;
+  }
+  return outputStr;
+}
 
 module.exports = reverse;
