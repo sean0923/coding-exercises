c4d09f90d9ae3fada74c0bbdbaf49fb76ea9be73
Sean Hong <iamseanhong@gmail.com>
2018-12-27 11:48:26 -0700

    steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,32 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+const addChars = (char, count) => {
+  let outputStr = '';
+  for (let i = 0; i < count; i++) {
+    outputStr += char;
+  }
+  return outputStr;
+};
+
+function steps(n) {
+  // looking at output ex) 4
+  //  looks like  1 # and 3 space
+  // 1 3
+  // 2 2
+  // 3 1
+  // 4 0
+  // so will use for loop start from stepCount 1 -> n
+  // spaceCount = n - stepCount
+  // will have add chars helper func (char, count)
+  // in for loop it will be
+  // printing addChars('#', idx) + addChars('#', spaceCount)
+
+  for (let stepCount = 1; stepCount <= n; stepCount++) {
+    const spaceCount = n - stepCount;
+    const str = addChars('#', stepCount) + addChars(' ', spaceCount);
+    console.log(str);
+  }
+}
 
 module.exports = steps;
