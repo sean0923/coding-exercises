0fe715c45ed4ba52e28ec8b922deb381c769c78a
Sean Hong <iamseanhong@gmail.com>
2018-12-27 09:46:20 -0700

    Solve max chars

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,27 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  // make counter with reduce
+  // make key value to nested arr of [objKey, objValue]
+  // sort from low to high
+  let charCounter = str.split('').reduce((acc, char) => {
+    if (acc[char] === undefined) {
+      acc[char] = 1;
+    } else {
+      acc[char] += 1;
+    }
+
+    return acc;
+  }, {});
+
+  let keyValuePairs = Object.entries(charCounter);
+
+  let sortedKeyValuePairs = keyValuePairs.sort((arrA, arrB) => {
+    return arrB[1] - arrA[1];
+  });
+
+  return sortedKeyValuePairs[0][0];
+}
 
 module.exports = maxChar;
--- exercises/temp.js
+++ exercises/temp.js
@@ -1,1 +1,27 @@
-console.log(Math.round(2.5));
+function maxChar(str) {
+  // make counter with reduce
+  // make key value to nested arr of [objKey, objValue]
+  // sort from low to high
+  let charCounter = str.split('').reduce((acc, char) => {
+    if (acc[char] === undefined) {
+      acc[char] = 1;
+    } else {
+      acc[char] += 1;
+    }
+
+    return acc;
+  }, {});
+  console.log('charCounter: ', charCounter);
+
+  let keyValuePairs = Object.entries(charCounter);
+  console.log('keyValuePairs: ', keyValuePairs);
+  let sortedKeyValuePairs = keyValuePairs.sort((arrA, arrB) => {
+    return arrB[1] - arrA[1];
+  });
+  console.log('sortedKeyValuePairs: ', sortedKeyValuePairs);
+
+  return sortedKeyValuePairs[0][0];
+}
+
+let str = 'abbccccda';
+maxChar(str);
