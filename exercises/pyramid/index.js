7ccf68574d29ad3a42c7b719fe2c63e277225160
Sean Hong <iamseanhong@gmail.com>
2018-12-28 19:26:27 -0700

    pyramid iterative solution

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,37 +14,61 @@
 //       ' ### '
 //       '#####'
 
-const getStrWithChar = (char, count) => {
-  let outputStr = '';
+// 1 -> 1
+// 2 -> 3
+// 3 -> 5
 
-  for (let i = 0; i < count; i++) {
-    outputStr += char;
-  }
-
-  return outputStr;
-};
-
+// * iterative
 function pyramid(n) {
-  // just observing
-  // 2 1 2
-  // 1 3 1
-  // 0 5 0
+  const maxCol = n * 2 - 1;
 
-  // 3 1 3 | 1 | n - f, just 1 odd num, n - 1
-  // 2 3 2 | 2 | n - f, just 2 odd num, n - 2
-  // 1 5 1 | 3 | n - f, just 3 odd num, n - 3
-  // 0 7 0 | 4 | n - f, just 4 odd num, n - 4
+  const colMidPoint = Math.floor(maxCol / 2);
 
-  // nOddNum
-  // n * 2 - 1
-  // 1 -> 1
-  // 2 -> 3
-  // 3 -> 5
-  for (let i = 1; i <= n; i++) {
-    const emptySpaces = getStrWithChar(' ', n - i);
-    const steps = getStrWithChar('#', i * 2 - 1);
-    console.log(emptySpaces + steps + emptySpaces);
+  for (let row = 0; row < n; row++) {
+    let str = '';
+    for (let col = 0; col < maxCol; col++) {
+      if (col >= colMidPoint - row && col <= colMidPoint + row) {
+        str += '#';
+      } else {
+        str += ' ';
+      }
+    }
+    console.log(str);
   }
 }
+
+// * my solution
+// const getStrWithChar = (char, count) => {
+//   let outputStr = '';
+
+//   for (let i = 0; i < count; i++) {
+//     outputStr += char;
+//   }
+
+//   return outputStr;
+// };
+
+// function pyramid(n) {
+//   // just observing
+//   // 2 1 2
+//   // 1 3 1
+//   // 0 5 0
+
+//   // 3 1 3 | 1 | n - f, just 1 odd num, n - 1
+//   // 2 3 2 | 2 | n - f, just 2 odd num, n - 2
+//   // 1 5 1 | 3 | n - f, just 3 odd num, n - 3
+//   // 0 7 0 | 4 | n - f, just 4 odd num, n - 4
+
+//   // nOddNum
+//   // n * 2 - 1
+//   // 1 -> 1
+//   // 2 -> 3
+//   // 3 -> 5
+//   for (let i = 1; i <= n; i++) {
+//     const emptySpaces = getStrWithChar(' ', n - i);
+//     const steps = getStrWithChar('#', i * 2 - 1);
+//     console.log(emptySpaces + steps + emptySpaces);
+//   }
+// }
 
 module.exports = pyramid;
