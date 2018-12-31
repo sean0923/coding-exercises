02aa8b8165371b841bf53a6fcb84069503dfd116
Sean Hong <iamseanhong@gmail.com>
2018-12-31 14:17:01 -0700

    fib

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,35 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+// * recursive
+function fib(n) {
+  // base
+  // n = 0 -> 0
+  // n = 1 -> 1
+
+  // return fib(n - 1) + fib(n - 2)
+
+  if (n === 0) return 0;
+  if (n === 1) return 1;
+  return fib(n - 1) + fib(n - 2);
+}
+
+// * iterative
+// function fib(n) {
+//   const nums = [0, 1];
+//   const idx = n;
+
+//   if (idx < 2) {
+//     return nums[idx];
+//   }
+
+//   let i = 2;
+//   while (i <= idx) {
+//     nums.push(nums[i - 2] + nums[i - 1]);
+//     i++;
+//   }
+
+//   return nums[idx];
+// }
 
 module.exports = fib;
