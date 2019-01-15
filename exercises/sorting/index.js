8cba4911dd02a3c5c2f76f81b8933c46b1099efd
Sean Hong <iamseanhong@gmail.com>
2019-01-14 22:03:06 -0800

    Solve bubble sort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -1,20 +1,33 @@
 // --- Directions
 // Implement bubbleSort, selectionSort, and mergeSort
 
+const swap = (arr, currIdx, nextIdx) => {
+  const temp = arr[currIdx];
+  arr[currIdx] = arr[nextIdx];
+  arr[nextIdx] = temp;
+  return arr;
+};
+
 function bubbleSort(arr) {
+  // from i: 0 -> arr.length - 2
+  for (let i = 0; i < arr.length - 1; i++) {
+    // from j : i + 1 -> arr.lengh
+    for (let j = i + 1; j < arr.length; j++) {
+      // if i > j
+      if (arr[i] > arr[j]) {
+        // swap
+        swap(arr, i, j);
+      }
+    }
+  }
 
+  return arr;
 }
 
-function selectionSort(arr) {
+function selectionSort(arr) {}
 
-}
+function mergeSort(arr) {}
 
-function mergeSort(arr) {
-
-}
-
-function merge(left, right) {
-
-}
+function merge(left, right) {}
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
--- exercises/sorting/test.js
+++ exercises/sorting/test.js
@@ -12,7 +12,7 @@ function getSortedArray() {
   return [-124, -40, 0, 7, 21, 100, 500];
 }
 
-describe('Bubble sort', () => {
+describe.only('Bubble sort', () => {
   test('sorts an array', () => {
     expect(bubbleSort(getArray())).toEqual(getSortedArray());
   });
@@ -29,9 +29,9 @@ describe('Merge sort', () => {
     const left = [1, 10];
     const right = [2, 8, 12];
 
-    expect(merge(left, right)).toEqual([1,2,8,10,12]);
+    expect(merge(left, right)).toEqual([1, 2, 8, 10, 12]);
   });
-  
+
   test('sorts an array', () => {
     expect(mergeSort(getArray())).toEqual(getSortedArray());
   });
