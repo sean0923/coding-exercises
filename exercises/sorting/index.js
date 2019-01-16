e759576710a6f7a7dfc5555f055358853cde9242
Sean Hong <iamseanhong@gmail.com>
2019-01-16 02:09:41 -0800

    Implement merge sort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -9,6 +9,29 @@ const swap = (arr, currIdx, nextIdx) => {
 };
 
 function bubbleSort(arr) {
+  // nextIdxLimit = arr.length
+  let nextIdxLimit = arr.length;
+
+  // while (nextIdxLimit !== 0)
+  while (nextIdxLimit !== 0) {
+    // for currIdx 0 -> nextIdxLimit - 1
+    for (let currIdx = 0; currIdx < nextIdxLimit - 1; currIdx++) {
+      // nextIdx = currIdx+1
+      const nextIdx = currIdx + 1;
+
+      // if(currNum > nextNum)
+      if (arr[currIdx] > arr[nextIdx]) {
+        swap(arr, currIdx, nextIdx);
+      }
+    }
+
+    nextIdxLimit--;
+  }
+
+  return arr;
+}
+
+function selectionSort(arr) {
   // from i: 0 -> arr.length - 2
   for (let i = 0; i < arr.length - 1; i++) {
     // from j : i + 1 -> arr.lengh
@@ -24,10 +47,47 @@ function bubbleSort(arr) {
   return arr;
 }
 
-function selectionSort(arr) {}
+function mergeSort(arr) {
+  // really split into smaller pieces
 
-function mergeSort(arr) {}
+  // if (arr.length === 1)
+  if (arr.length === 1) {
+    // return arr
+    return arr;
+  }
 
-function merge(left, right) {}
+  // midIdx
+  const midIdx = Math.floor(arr.length / 2);
+
+  // leftArr
+  const leftArr = arr.slice(0, midIdx);
+  // rightArr
+  const rightArr = arr.slice(midIdx);
+
+  return merge(mergeSort(leftArr), mergeSort(rightArr));
+}
+
+function merge(left, right) {
+  // assume left and right is sorted
+
+  // storage arr
+  const storage = [];
+
+  // while both length !== 0
+  while (left.length && right.length) {
+    // if first element of right smaller
+    if (left[0] < right[0]) {
+      // shift right and push to storage
+      storage.push(left.shift());
+    } else {
+      // else
+      // shift left and push to storage
+      storage.push(right.shift());
+    }
+  }
+
+  // return [...storage, ...left, ...right]
+  return [...storage, ...left, ...right];
+}
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
--- exercises/sorting/test.js
+++ exercises/sorting/test.js
@@ -12,7 +12,7 @@ function getSortedArray() {
   return [-124, -40, 0, 7, 21, 100, 500];
 }
 
-describe.only('Bubble sort', () => {
+describe('Bubble sort', () => {
   test('sorts an array', () => {
     expect(bubbleSort(getArray())).toEqual(getSortedArray());
   });
